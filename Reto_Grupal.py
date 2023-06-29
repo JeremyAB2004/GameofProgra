28435b5a824478d659b219e05a25db3adc834775
JeremyAB2004 <134981147+JeremyAB2004@users.noreply.github.com>
2023-06-28 22:39:51 -0600

    Se agrega menu

--- Reto_Grupal.py
+++ Reto_Grupal.py
@@ -1,2 +1,16 @@
 #Ejercicio #1 de Game of Progra
-
+#Menu
+#Variables
+conteoPersonas = 1
+estadisticaEdades = 2
+salir = 0
+#Obciones del menu
+menuPrincipal = int(input("Menu Principal: \n 1-Ingresar una edad. \n 2-Ver la estadistica. \n 0-Salir.\n"))
+while menuPrincipal !=0:
+    if menuPrincipal == 1:
+        #aqui va el codigo del conteo
+    elif menuPrincipal == 2:
+        #aqui va el codigo de la estadistica
+    else: 
+        print ("Digita una opcion correcta.")
+    menuPrincipal = int(input("Menu Principal: \n 1-Ingresar una edad. \n 2-Ver la estadistica. \n 0-Salir.\n"))
