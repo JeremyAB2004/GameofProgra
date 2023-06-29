c2bd963b5f616b02337d5498afaba202d4beda9d
EduardoCR1994 <133717717+EduardoCR1994@users.noreply.github.com>
2023-06-28 22:48:34 -0600

    Update Reto_Grupal.py

--- Reto_Grupal.py
+++ Reto_Grupal.py
@@ -1,6 +1,7 @@
 #Ejercicio #1 de Game of Progra
 #Conteo de personas.
 #Variables
+personas = 0
 edad = 0
 menor = 0 
 adulto = 0
@@ -11,10 +12,13 @@ while ingreseOtra == "si" or ingreseOtra == "s":
   edad = int(input("Ingrese su edad: "))
   if edad < 18:
     menor += 1
+    personas += 1
     ingreseOtra = input("Desea agregar otra edad (si o no):\n")
   elif edad in range(17,66):
     adulto += 1
+    personas += 1
     ingreseOtra = input("Desea agregar otra edad (si o no):\n")
   else:
     mayor += 1
+    personas += 1
     ingreseOtra = input("Desea agregar otra edad (si o no):\n")
