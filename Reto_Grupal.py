76f2a45c5b2c5415d4636238957886d0d6bfbeb1
EduardoCR1994 <133717717+EduardoCR1994@users.noreply.github.com>
2023-06-28 22:09:57 -0600

    Contador de edades

--- Reto_Grupal.py
+++ Reto_Grupal.py
@@ -1,2 +1,20 @@
 #Ejercicio #1 de Game of Progra
-
+#Conteo de personas.
+#Variables
+edad = 0
+menor = 0 
+adulto = 0
+mayor = 0
+ingreseOtra = "si"
+#conteo de las personas 
+while ingreseOtra == "si" or ingreseOtra == "s":
+  edad = int(input("Ingrese su edad: "))
+  if edad < 18:
+    menor += 1
+    ingreseOtra = input("Desea agregar otra edad (si o no):\n")
+  elif edad in range(17,66):
+    adulto += 1
+    ingreseOtra = input("Desea agregar otra edad (si o no):\n")
+  else:
+    mayor += 1
+    ingreseOtra = input("Desea agregar otra edad (si o no):\n")
