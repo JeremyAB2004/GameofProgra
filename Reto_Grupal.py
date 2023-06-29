c059db28fd6cb3da1b383b58bdceb05df7e0bbec
JeremyAB2004 <134981147+JeremyAB2004@users.noreply.github.com>
2023-06-28 22:40:26 -0600

    Se agrega conteo

--- Reto_Grupal.py
+++ Reto_Grupal.py
@@ -8,7 +8,25 @@ salir = 0
 menuPrincipal = int(input("Menu Principal: \n 1-Ingresar una edad. \n 2-Ver la estadistica. \n 0-Salir.\n"))
 while menuPrincipal !=0:
     if menuPrincipal == 1:
-        #aqui va el codigo del conteo
+        #Conteo de personas.
+        #Variables
+        edad = 0
+        menor = 0 
+        adulto = 0
+        mayor = 0
+        ingreseOtra = "si"
+        #conteo de las personas 
+        while ingreseOtra == "si" or ingreseOtra == "s":
+          edad = int(input("Ingrese su edad: "))
+          if edad < 18:
+            menor += 1
+            ingreseOtra = input("Desea agregar otra edad (si o no):\n")
+          elif edad in range(17,66):
+            adulto += 1
+            ingreseOtra = input("Desea agregar otra edad (si o no):\n")
+          else:
+            mayor += 1
+            ingreseOtra = input("Desea agregar otra edad (si o no):\n")
     elif menuPrincipal == 2:
         #aqui va el codigo de la estadistica
     else: 
